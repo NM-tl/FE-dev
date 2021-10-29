989201ee2b7f73139f56d3abe26a5b48b11e71de
Nik <30575825+NM-tl@users.noreply.github.com>
2021-10-29 15:48:59 +0300

    Add files via upload

--- lesson9/js/main.js
+++ lesson9/js/main.js
@@ -19,34 +19,30 @@ const universes = [
   ['❤️', 'Marvel', ['Iron Man', 'the Hulk', 'Black Widow']],
 ];
 
-function getInfo(arr, str) {
-  const TRs = [];
+const buildTd = (td, separator = '; ') =>
+  `<td>${Array.isArray(td) ? td.join(separator) : td}</td>`;
 
-  for (let i = 0; i < arr.length; i++) {
-    let td = '';
+const buildTdList = (tds) =>
+  Array.isArray(tds) ? tds.map((item) => buildTd(item)).join('') : buildTd(tds);
 
-    for (let j = 0; j < arr[i].length; j++) {
-      td += `<td>${arr[i][j]}</td>`;
+const buildTr = (tr) => `<tr>${buildTdList(tr)}</tr>`;
 
-      TRs.push(`<tr>${td}</tr>`);
-    }
+const buildTrList = (trs) =>
+  Array.isArray(trs) ? trs.map((item) => buildTr(item)) : buildTr(trs);
 
-    return `<caption>${str} info</caption><table>${TRs.join('')}</table>`;
-  }
+function buildForm(arr, str) {
+  return `<caption>${str} info</caption><table>${buildTrList(arr).join(
+    '',
+  )}</table>`;
 }
 
-function deepCopeArray(arr) {
-  return arr.map((item) => (Array.isArray(item) ? deepCopeArray(item) : item));
-}
+console.log(buildForm(universes), 'Test');
 
-const test = deepCopeArray(universes);
+// function deepCopeArray(arr) {
+//   return arr.map((item) => (Array.isArray(item) ? deepCopeArray(item) : item));
+// }
 
-test[0].push(1);
-
-console.log(test);
-
-
-//filter isArray \ for
+// filter isArray \ for
 // function copiedArray(arr) {
 //   const newArr = [];
 //
@@ -62,9 +58,107 @@ console.log(test);
 // copiedArray(universes);
 
 // const copiedArr = newArr;
-console.log(universes);
+document.write(buildForm(animals, 'Test'));
+document.write(buildForm(food, 'Food'));
+document.write(buildForm(universes, 'Universes'));
 
-document.write(getInfo(animals, 'Animals'));
+// const sss = arr.map((i) => {
+//   let td = '';
+//   return `<tr>${i.map((j) => (td += `<td>${j}</td>`))}</tr>`;
+// });
+
+// let arr = [
+// 	[1,2,3,[[true,false],'dog']],
+// 	[4,5,6],
+// 	7
+// ];
+//
+// function copiedArray(arr){ // [true,false]
+// 	let newArr = [];
+//
+// 	for(let i=0; i<arr.length; i++){
+// 		if(Array.isArray(arr[i])){
+// 			newArr.push( copiedArray(arr[i]) ); // [1,2,3]
+// 		} else{
+// 			newArr.push(arr[i]);
+// 		}
+// 	}
+//
+// 	return newArr;
+// }
+//
+// let copyOfArr = copiedArray(arr);
+//
+// console.log(arr);
+// console.log(copyOfArr);
+
+
+
+// var
+
+
+const animals = [
+  ['🐭', 'mouse', 'Jerry'],
+  ['🐹', 'hamster', 'Biscuit'],
+  ['🐰', 'rabbit', 'Bugs'],
+  ['🦊', 'fox', 'Mrs. Fox'],
+  ['🐻', 'bear', 'Paddington'],
+];
+
+const food = [
+  ['🍎', 'apple', 10],
+  ['🍐', 'pear', 12],
+  ['🍊', 'tangerine', 15],
+  ['🍋', 'lemon', 5],
+  ['🍌', 'banana', 7],
+];
+
+const universes = [
+  ['🖤', 'DC', ['Superman', 'Batman', 'Wonder Woman']],
+  ['❤️', 'Marvel', ['Iron Man', 'the Hulk', 'Black Widow']],
+];
+
+const getTd = (value, separator = '; ') =>
+    `<td>${Array.isArray(value) ? value.join(separator) : value}</td>`;
+
+function getInfo(arr, str) {
+  const TRs = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let td = '';
+
+    for (let j = 0; j < arr[i].length; j++) {
+      td += getTd(arr[i][j]);
+    }
+    TRs.push(`<tr>${td}</tr>`);
+  }
+
+  return `<caption>${str} info</caption><table>${TRs.join('')}</table>`;
+}
+
+console.log(getInfo(universes), 'Test');
+
+// function deepCopeArray(arr) {
+//   return arr.map((item) => (Array.isArray(item) ? deepCopeArray(item) : item));
+// }
+
+// filter isArray \ for
+// function copiedArray(arr) {
+//   const newArr = [];
+//
+//   for (let i = 0; i < arr.length; i++) {
+//     if (Array.isArray(arr[i])) {
+//       newArr.push(copiedArray(arr[i]));
+//     } else {
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   return newArr;
+// }
+// copiedArray(universes);
+
+// const copiedArr = newArr;
+document.write(getInfo(animals, 'Test'));
 document.write(getInfo(food, 'Food'));
 document.write(getInfo(universes, 'Universes'));
 
