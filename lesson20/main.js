b477fbb70100cdb2328b9a4b9d073e66d7824e18
Nik <30575825+NM-tl@users.noreply.github.com>
2022-01-13 17:01:50 +0200

    Update 13.01.2022

--- lesson20/main.js
+++ lesson20/main.js
@@ -2,9 +2,10 @@ const block = document.querySelector(`.block`);
 const maxOffsetLeft = window.innerWidth - block.offsetWidth;
 const maxOffsetTop = window.innerHeight - block.offsetHeight;
 const STEP = 20;
-console.log(window.innerWidth, maxOffsetLeft)
+const JUMP = 50;
 
 document.onkeydown = event => {
+    console.log(event)
     if(event.keyCode === 39){
         let nextOffsetLeft = block.offsetLeft + STEP;
 
@@ -21,26 +22,26 @@ document.onkeydown = event => {
     if(event.keyCode === 37){
         let nextOffsetLeft = block.offsetLeft - STEP;
 
-        if(nextOffsetLeft > maxOffsetLeft){
+        if(nextOffsetLeft < 0){
             block.style.left = `${parseInt(block.style.left) + STEP*2}px`;
 
             block.classList.add(`bems`);
             setTimeout(()=> block.classList.remove(`bems`), 1000);
         } else{
-            block.style.left = !block.style.left ? `${STEP}px` : `${parseInt(block.style.left) - STEP}px`
+            block.style.left = !block.style.left ? `${-STEP}px` : `${parseInt(block.style.left) - STEP}px`
         }
     }
 
     if(event.keyCode === 38){
         let nextOffsetTop = block.offsetTop - STEP;
 
-        if(nextOffsetTop > maxOffsetTop){
+        if(nextOffsetTop < 0){
             block.style.top = `${parseInt(block.style.top) + STEP*2}px`;
 
             block.classList.add(`bems`);
             setTimeout(()=> block.classList.remove(`bems`), 1000);
         } else{
-            block.style.top = !block.style.top ? `${STEP}px` : `${parseInt(block.style.top) - STEP}px`
+            block.style.top = !block.style.top ? `${-STEP}px` : `${parseInt(block.style.top) - STEP}px`
         }
     }
 
@@ -58,24 +59,34 @@ document.onkeydown = event => {
     }
 
     if(event.keyCode === 32){
-        const Jump = 10;
-        nextOffsetTop = `${parseInt(block.offsetTop) + Jump}px`;
-        // setTimeout(()=> block.classList.remove(`bems`), 1000);
+        block.style.top = !block.style.top ? `${-JUMP}px` : `${parseInt(block.style.top) - JUMP}px`
+        setTimeout(()=> block.style.top = '', 1000);
+    }
+
+    if(event.keyCode === 17){
+        block.offsetWidth += `${25}%`;
+        // block.offsetHeight = `${parseInt(block.offsetHeight) - 40}%`;
+        console.log(block.offsetWidth)
     }
 }
+console.log(block.offsetHeight)
+console.log(block.offsetWidth)
 
 
-// document.onkeydown = event => {
-//     console.log(event);
+
+// window.onresize = () => {
+//     maxOffsetLeft(window.innerWidth);
+//     maxOffsetTop(window.innerHeight);
+//     console.log(window.innerWidth);
+//     console.log(window.innerHeight);
 // }
-
-
-// window.addEventListener('keydown', (event) => {
-//     const { style } = block;
-//     switch (event.key) {
-//         case 'ArrowUp': style.top = `${parseInt(style.top) - STEP}px`; break;
-//         case 'ArrowDown': style.top = `${parseInt(style.top) + STEP}px`; break;
-//         case 'ArrowLeft': style.left = `${parseInt(style.left) - STEP}px`; break;
-//         case 'ArrowRight': style.left = `${parseInt(style.left) + STEP}px`; break;
+//
+// const MOBILE = 375;
+//
+// block.onclick = () => {
+//     if(window.innerWidth <= MOBILE){
+//         console.log('mobile func');
+//     } else{
+//         console.log('desktop func');
 //     }
-// });
+// }
